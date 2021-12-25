dba4dd1b6e6873ce7bcdc981e083545449c7f0e8
objectMaker <528154320@qq.com>
2021-12-26 00:24:31 +0800

    '生成随机20张图片'

--- src/mockjs/index.js
+++ src/mockjs/index.js
@@ -1,6 +1,25 @@
 import Mock from 'mockjs';
 
 Mock.mock(/\/api\/images/,function (params){
+    let sizeArr = [
+        '300x250', '250x250', '240x400', '336x280',
+        '180x150', '720x300', '468x60', '234x60',
+        '88x31', '120x90', '120x60', '120x240',
+        '125x125', '728x90', '160x600', '120x600',
+        '300x600'
+    ]
+    let imageArr = [];
+    for (let i=0;i<20;i++){
+        let info = sizeArr[Math.floor(Math.random()*sizeArr.length)]
+        let imageUrl = Mock.Random.image(info)
+        let width = info.split('x')[0]
+        let height = info.split('x')[1]
+        imageArr.push({
+            imageUrl,
+            width,
+            height
+        })
+    }
     console.log(params)
-    return Mock.Random.image()
+    return imageArr
 })
